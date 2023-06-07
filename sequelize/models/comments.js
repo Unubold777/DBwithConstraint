854abf97babe2df6250d6dbbc1e6bd41a66fd560
Unux7 <unuuthesupp123@gmail.com>
2023-06-07 11:46:22 +0800

    initial commit

---
+++ sequelize/models/comments.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class comments extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  comments.init({
+    pollid: DataTypes.INTEGER,
+    userid: {
+      type:DataTypes.INTEGER,
+      allowNull:true},
+    comment: DataTypes.STRING,
+    posteddate: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'comments',
+  });
+  comments.associate = model =>{
+    comments.belongsTo(models.users,{
+      foreignKey:'userid',
+      onDelete:'SET NULL'
+    });
+    comments.belongsTo(models.polls,{
+      foreignKey:'pollid',
+      onDelete:'CASCADE'
+    })
+  }
+  return comments;
+};
