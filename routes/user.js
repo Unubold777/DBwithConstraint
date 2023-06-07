5c9f6838ab2d1a6ac5b80bc66bf5781840f69371
Zvkkaaa <zvkkaaa@gmail.com>
2023-06-07 15:53:15 +0800

    written controller for polls

---
+++ routes/user.js
@@ -0,0 +1,7 @@
+const { uploadImageProfile, createUser } = require("../controller/user");
+const { protect } = require("../middleware/protect");
+
+const router = require("express").Router();
+
+router.route("/create").post(createUser);
+module.exports = router;
