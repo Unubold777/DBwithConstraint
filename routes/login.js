284f7883ccfc08537f34fa63e41b2d0d82591b52
Unux7 <unuuthesupp123@gmail.com>
2023-06-12 09:37:59 +0800

    updating folders in main

--- routes/login.js
+++
@@ -1,8 +0,0 @@
-const { Login } = require("../controller/login");
-const { protect } = require("../middleware/protect");
-
-const router = require("express").Router();
-
-router.route("/login").post(Login);
-
-module.exports = router;
--- routes/user.js
+++
@@ -1,7 +0,0 @@
-const { uploadImageProfile, createUser } = require("../controller/user");
-const { protect } = require("../middleware/protect");
-
-const router = require("express").Router();
-
-router.route("/create").post(createUser);
-module.exports = router;
